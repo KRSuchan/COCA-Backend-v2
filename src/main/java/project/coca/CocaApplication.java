a16573cc5791f27f1b203a84a696453737a25744
KRSuchan <lsc1814@naver.com>
2025-04-23 11:26:49 +0900

    ci : github actions update test

--- src/main/java/project/coca/CocaApplication.java
+++ src/main/java/project/coca/CocaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CocaApplication {
-    // ci test v2504231120
+    // ci test v2504231126
     public static void main(String[] args) {
         SpringApplication.run(CocaApplication.class, args);
     }
