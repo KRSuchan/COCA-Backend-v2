7059f4cebb3e69f8d556a8ee3f5bf8ff124cd004
KRSuchan <lsc1814@naver.com>
2025-04-26 21:27:20 +0900

    cd : backend update test

--- src/main/java/project/coca/CocaApplication.java
+++ src/main/java/project/coca/CocaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CocaApplication {
-    // ci test v2504231126
+    // ci test v2504262127
     public static void main(String[] args) {
         SpringApplication.run(CocaApplication.class, args);
     }
